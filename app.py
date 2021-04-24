3ce871126f027cbf9cbd936b36b626c978845aa7
nola <1422450420@qq.com>
2021-04-24 21:14:44 +0800

    add():use bootstrap plugin

--- app.py
+++ app.py
@@ -3,8 +3,10 @@
 # @Author : Nola
 # @File : app
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
